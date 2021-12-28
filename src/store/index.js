81aecbde5382772aa38968897af29acf6ac871cd
qaqqiong <96399091+qaqqiong@users.noreply.github.com>
2021-12-28 09:07:47 +0800

    创建项目，简化项目框架，安装项目所需的依赖

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import Vue from "vue"
+//引入vuex
+import Vuex from "vuex"
+import mutations from '@/store/mutations'
+//挂载vuex
+Vue.use(Vuex)
+    //创建vuex对象
+const store = new Vuex.Store({
+        state: {
+            //此处的键值对就是vuex要管理的状态
+        },
+        //方法 用来给数据赋值
+        mutations,
+    })
+    //导出store
+export default store
